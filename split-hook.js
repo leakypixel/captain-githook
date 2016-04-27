abce6a0eb67cfe46defd2d016c304ac4aa9a745e
Craig Fletcher <hi@leakypixel.net>
2016-04-27 17:45:48 +0100

    Pipe exit codes through to calling shell

--- split-hook.js
+++ split-hook.js
@@ -8,13 +8,21 @@ var exec = require("child_process").exec;
 var callingFile = path.basename(__filename);
 var hooksDir = path.dirname(__filename);
 
+// If one of our hooks exits with a code other than zero, we want to pipe that
+// through to our calling shell.
+function handleExitCode(error) {
+  if (error !== null && error.code !== 0) {
+      process.exit(error.code);
+  }
+}
+
 // We call local hooks first. We're doing this because we want our shared hooks
 // to (probably) validate against styleguides etc. - no use if a local hook can
 // change stuff *after* we've checked it.
 var localHook = path.join(hooksDir, "local", callingFile);
 fs.stat(localHook, function(err, stats) {
   if (stats && stats.isFile()) {
-    var localHookProcess = exec(localHook);
+    var localHookProcess = exec(localHook, handleExitCode);
     process.stdin.pipe(localHookProcess.stdin);
     localHookProcess.stdout.pipe(process.stdout);
     localHookProcess.stderr.pipe(process.stderr);
@@ -24,7 +32,7 @@ fs.stat(localHook, function(err, stats) {
 var sharedHook = path.join(hooksDir, "shared", callingFile);
 fs.stat(sharedHook, function(err, stats) {
   if (stats && stats.isFile()) {
-    var sharedHookProcess = exec(sharedHook);
+    var sharedHookProcess = exec(sharedHook, handleExitCode);
     process.stdin.pipe(sharedHookProcess.stdin);
     sharedHookProcess.stdout.pipe(process.stdout);
     sharedHookProcess.stderr.pipe(process.stderr);
